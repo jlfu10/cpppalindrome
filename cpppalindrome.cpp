6216eb1c251073057b66d10949acdb29fb821ee0
jlfudeveloper <69946005+jlfudeveloper@users.noreply.github.com>
2020-09-16 11:26:09 -0700

    comments

--- cpppalindrome.cpp
+++ cpppalindrome.cpp
@@ -4,25 +4,30 @@
 #include <cctype>
 using namespace std;
 
-int main(){
-  char phrase[80];
-  char modphrase[80];
-  bool palindrome = true;
-  int count = 0;
-  cout << "enter a phrase: " << endl;
+//still need to add different case checking
+int main(){ 
+  char phrase[80]; //initiated cstring to get input
+  char modphrase[80]; //initiated modified cstring to only accept valid chars
+  bool palindrome = true; //is the phrase a palindrome
+  int count = 0; //for adding to modphrase
+  cout << "enter a phrase: " << endl; 
   cin.get(phrase, 80, '\n');
   cin.get();
+  //gets phrase
   for(int i = 0;i < 80; i++){
     if( (int(phrase[i]) >= 65 && int(phrase[i]) <= 95) || (int(phrase[i]) >= 97 && int(phrase[i]) <= 122)){
+      //is the phrase a valid character? if so, add it to modphrase
       modphrase[count] = phrase[i];
       count++;
     }
   }
+  //loop through modphrase fowards and backwards and compare
   for(int i = 0; i < count; i++){
     if(modphrase[i] != modphrase[count-i-1]){
-      palindrome = false;
+      palindrome = false; //if at any point characters are different, it is not a palindrome
     }
   }
+  //tell the user
   if(palindrome){
     cout << "palindrome!" << endl;
   }
