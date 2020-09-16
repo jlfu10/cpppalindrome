0be2e5e258dbe95815de7f3a6307cef13096a06b
jlfudeveloper <69946005+jlfudeveloper@users.noreply.github.com>
2020-09-15 17:33:28 -0700

    tried to implement stripping the phrase
    stripping = getting rid of characters that are not letters.
    did not work.

--- cpppalindrome.cpp
+++ cpppalindrome.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
 #include <cstring>
-
+#include <vector>
 using namespace std;
 
 int main(){
-  cout << "cpppalindrome" << endl;
+  char phrase[80];
+  vector<char> modphrase;
+  cout << "enter a phrase: " << endl;
+  cin.get(phrase, 80, '\n');
+  for(int i = 0;i < 80; i++){
+    phrase[i].toupper();
+    if(int(phrase[i]) >= 65 && int(phrase[i]) <= 90){
+      modphrase.push_back(phrase[i]);
+    }
+  }
+  for(auto x: modphrase){
+    cout << x << endl;
+  }
   return 0;
 }
