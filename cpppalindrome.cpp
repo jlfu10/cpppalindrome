0a692d5828610c2cf3e05d55abdd67c2b370745e
jlfudeveloper <69946005+jlfudeveloper@users.noreply.github.com>
2020-09-16 11:04:46 -0700

    confusion

--- cpppalindrome.cpp
+++ cpppalindrome.cpp
@@ -1,21 +1,32 @@
 #include <iostream>
 #include <cstring>
-#include <vector>
+#include <cctype>
 using namespace std;
 
 int main(){
   char phrase[80];
-  vector<char> modphrase;
+  char modphrase[80];
+  int moddex = 0;
+  bool palindrome = true;
   cout << "enter a phrase: " << endl;
   cin.get(phrase, 80, '\n');
   for(int i = 0;i < 80; i++){
-    phrase[i].toupper();
-    if(int(phrase[i]) >= 65 && int(phrase[i]) <= 90){
-      modphrase.push_back(phrase[i]);
+    if(int(toupper(phrase[i])) >= 65 && int(toupper(phrase[i])) <= 90){
+      modphrase[moddex] = phrase[i];
+      moddex += 1;
     }
   }
-  for(auto x: modphrase){
-    cout << x << endl;
+  for(int i = 0; i < moddex; i++){
+    if(toupper(modphrase[i]) != toupper(modphrase[moddex-i-1])){
+      cout << modphrase[i]<< modphrase[moddex-i-1] << endl;
+      palindrome = false;
+    }
   }
+  if(palindrome){
+    cout << "palindrome!" << endl;
+  }
+  else{
+    cout << "not palindrome!" << endl;
+  }  
   return 0;
 }
